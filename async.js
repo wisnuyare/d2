3081972a3be102f0a0ba477630ae38facef2514b
wisnuyare <wisnuyare@gmail.com>
2018-08-15 07:11:23 +0700

    promise added

--- async.js
+++ async.js
@@ -1,22 +1,35 @@
 window.onload = function () {
 
-    function get(url){
-        return new Promise(function(resolve, reject){
+    function get(url) {
+        return new Promise(function (resolve, reject) {
             let xhttp = new XMLHttpRequest();
             xhttp.open("GET", url, true);
-            xhttp.onload = function(){
-                if(xhttp.status == 200){
+            xhttp.onload = function () {
+                if (xhttp.status == 200) {
                     resolve(JSON.parse(xhttp.response));
-                }else{
+                } else {
                     reject(xhttp.statusText);
                 }
             };
-            xhttp.onerror = function(){
+            xhttp.onerror = function () {
                 reject(xhttp.statusText);
             };
             xhttp.send();
         });
-    }
+    };
+    let promise = get("data/tweets.json");
+    promise.then(function (tweets) {
+        console.log(tweets);
+        return get("data/friends.json");
+    }).then(function(friends){
+        console.log(friends);
+        return get("data/videos.json");
+    }).then(function(friends){
+        console.log(videos);
+    }).catch(function(error){
+        console.log(error);
+    });
+
 
     // function handleError(jqXHR, textStatus, error){
     //     console.log(error);        
