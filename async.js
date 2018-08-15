4291a6b2b5c13fbf4f446d3b37a8298b0179bd70
wisnuyare <wisnuyare@gmail.com>
2018-08-15 07:19:08 +0700

    jquery with promise

--- async.js
+++ async.js
@@ -1,34 +1,44 @@
 window.onload = function () {
 
-    function get(url) {
-        return new Promise(function (resolve, reject) {
-            let xhttp = new XMLHttpRequest();
-            xhttp.open("GET", url, true);
-            xhttp.onload = function () {
-                if (xhttp.status == 200) {
-                    resolve(JSON.parse(xhttp.response));
-                } else {
-                    reject(xhttp.statusText);
-                }
-            };
-            xhttp.onerror = function () {
-                reject(xhttp.statusText);
-            };
-            xhttp.send();
-        });
-    };
-    let promise = get("data/tweets.json");
-    promise.then(function (tweets) {
+    $.get("data/tweets.json").then(function(tweets){
         console.log(tweets);
-        return get("data/friends.json");
+        return $.get("data/friends.json");
     }).then(function(friends){
         console.log(friends);
-        return get("data/videos.json");
-    }).then(function(friends){
-        console.log(videos);
-    }).catch(function(error){
-        console.log(error);
-    });
+        return  $.get("data/videos.json");        
+    }).then(function(videos){
+        console.log(videos);        
+    })
+
+    // function get(url) {
+    //     return new Promise(function (resolve, reject) {
+    //         let xhttp = new XMLHttpRequest();
+    //         xhttp.open("GET", url, true);
+    //         xhttp.onload = function () {
+    //             if (xhttp.status == 200) {
+    //                 resolve(JSON.parse(xhttp.response));
+    //             } else {
+    //                 reject(xhttp.statusText);
+    //             }
+    //         };
+    //         xhttp.onerror = function () {
+    //             reject(xhttp.statusText);
+    //         };
+    //         xhttp.send();
+    //     });
+    // };
+    // let promise = get("data/tweets.json");
+    // promise.then(function (tweets) {
+    //     console.log(tweets);
+    //     return get("data/friends.json");
+    // }).then(function(friends){
+    //     console.log(friends);
+    //     return get("data/videos.json");
+    // }).then(function(friends){
+    //     console.log(videos);
+    // }).catch(function(error){
+    //     console.log(error);
+    // });
 
 
     // function handleError(jqXHR, textStatus, error){
